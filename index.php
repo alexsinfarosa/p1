42e0c48065659015dda8f4d0ebaf373bb8286e7b
alexsinfarosa <alex.sinfarosa@me.com>
2015-01-30 00:22:39 -0500

    init

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php include 'incs/header.php'; ?>	
+	
+	<!-- HEADER -->
+
+
+
+	<!-- MAIN -->
+<section class="container">
+		<div class="col-3 orange">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate maxime facilis repudiandae tempore ipsam illum sunt nam, nihil sint, unde, vero incidunt quam doloremque quod molestiae qui voluptatum ratione adipisci.</p>
+		</div>
+		<div class="col-3">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate maxime facilis repudiandae tempore ipsam illum sunt nam, nihil sint, unde, vero incidunt quam doloremque quod molestiae qui voluptatum ratione adipisci.</p>
+		</div>
+		<div class="col-3 orange">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate maxime facilis repudiandae tempore ipsam illum sunt nam, nihil sint, unde, vero incidunt quam doloremque quod molestiae qui voluptatum ratione adipisci.</p>
+		</div>	
+</section>
+
+
+
+
+
+
+
+
+<?php include 'incs/footer.php'; ?>
